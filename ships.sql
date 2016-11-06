ca077fbdbcdb095aa0848db31587103c1f1be699
Daniel Dimitrov <bgdppeu@gmail.com>
2016-11-06 14:55:46 +0200

    Added counter

--- ships.sql
+++ ships.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS `ships` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username_1` text NOT NULL,
   `username_2` text NOT NULL,
+  `counter` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
